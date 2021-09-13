a616c08cd86f7a0b42affe0c441df2ff6c0669d3
Gil Katz <gil.katz@beamapp.co>
2021-09-13 17:39:54 +0100

    same for Quick and Nimble

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/alexandertar/LASwift", .branch("master")),
-        .package(url: "https://github.com/Quick/Quick.git", from: "2.2.1"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "8.1.2")
+        .package(url: "https://github.com/Quick/Quick.git", .branch("master")),
+        .package(url: "https://github.com/Quick/Nimble.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
