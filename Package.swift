43a9d5f8128acef3362af1af80043d11809f6052
Gil Katz <gil.katz@beamapp.co>
2021-09-13 11:04:24 +0100

    adding CI yml

--- Package.swift
+++ Package.swift
@@ -4,25 +4,32 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
+    name: "Clustering",
+    platforms: [
+        .macOS("10.14")
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]),
+            name: "Clustering",
+            targets: ["Clustering"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/alexandertar/LASwift", from: "0.2.5"),
+        .package(url: "https://github.com/Quick/Quick.git", from: "2.2.1"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "8.1.2")
+
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "MyLibrary",
-            dependencies: []),
+            name: "Clustering",
+            dependencies: ["LASwift"]),
         .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
+            name: "ClusteringTests",
+            dependencies: ["Clustering", "Nimble"])
     ]
 )
